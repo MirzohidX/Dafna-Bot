aec69b599b7aefc7e505361fc2c0bf750924c065
Dilshodbek <95227338+DilshodbekAhmedov@users.noreply.github.com>
2022-01-16 22:35:03 +0500

    yangilandi

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,9 +1,11 @@
 from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
+from filters import IsPrivate
 
 from loader import dp
 
 
-@dp.message_handler(CommandStart())
+@dp.message_handler(IsPrivate(), CommandStart())
 async def bot_start(message: types.Message):
-    await message.answer(f"Salom, {message.from_user.full_name} wikipedia botga xush kelibsiz!")
+    await message.answer(f"Assalomu alaykum, {message.from_user.full_name} wikipedia botga xush kelibsiz!")
+    await message.answer(f"O'zingizga kerakli ma'lumotni izlab toping")
